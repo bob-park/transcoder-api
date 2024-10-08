f9e01c4f6c5c9d9d1bc299b6c94c67b27c16f3ee
Bob Park <hwpark11@kakao.com>
2024-10-08 19:56:55 +0900

    Initialize Project

---
+++ src/main/java/org/bobpark/transcoder/configure/AppConfiguration.java
@@ -0,0 +1,7 @@
+package org.bobpark.transcoder.configure;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+}
---
+++ src/main/java/org/bobpark/transcoder/configure/transcoder/TranscoderConfiguration.java
@@ -0,0 +1,10 @@
+package org.bobpark.transcoder.configure.transcoder;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class TranscoderConfiguration {
+}
